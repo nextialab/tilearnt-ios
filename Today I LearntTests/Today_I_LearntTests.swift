03e82851521f976cefdb968c0c84d8d76c6346a1
Nelson Iván González <nigm2005@gmail.com>
2016-04-18 01:16:19 -0500

    First files

---
+++ Today I LearntTests/Today_I_LearntTests.swift
@@ -0,0 +1,36 @@
+//
+//  Today_I_LearntTests.swift
+//  Today I LearntTests
+//
+//  Created by Nelson Iván González on 4/18/16.
+//  Copyright © 2016 Nelson Iván González. All rights reserved.
+//
+
+import XCTest
+@testable import Today_I_Learnt
+
+class Today_I_LearntTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
