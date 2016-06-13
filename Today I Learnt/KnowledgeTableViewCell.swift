b902b7f363e0ed7049038f2bace99b8196c5657e
Nelson Iván González <nigm2005@gmail.com>
2016-06-13 17:43:43 -0500

    Adding Core Data

---
+++ Today I Learnt/KnowledgeTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  KnowledgeTableViewCell.swift
+//  Today I Learnt
+//
+//  Created by Nelson Iván González on 5/5/16.
+//  Copyright © 2016 Nelson Iván González. All rights reserved.
+//
+
+import UIKit
+
+class KnowledgeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Today I Learnt/ViewController.swift
+++ Today I Learnt/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var edit: UITextField!
+    var knowledge = [NSManagedObject]()
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: "Knowledge")
+        do {
+            let results = try managedContext.executeFetchRequest(fetchRequest)
+            knowledge = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        table.dataSource = self
+        edit.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if let text = textField.text {
+            addKnowledge(text)
+        }
+        textField.text = ""
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return knowledge.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "KnowledgeTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! KnowledgeTableViewCell
+        cell.label.text = (knowledge[indexPath.item].valueForKey("text") as! String)
+        return cell
+    }
 
+    func addKnowledge(newKnowledge: String) {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let entity = NSEntityDescription.entityForName("Knowledge", inManagedObjectContext: managedContext)
+        let entityKnowledge = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        entityKnowledge.setValue(newKnowledge, forKey: "text")
+        do {
+            try managedContext.save()
+            knowledge.append(entityKnowledge)
+            table.reloadData()
+        } catch let error as NSError {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
 }
 
